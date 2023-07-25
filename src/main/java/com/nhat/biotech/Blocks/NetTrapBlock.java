ea03e38d70b1ed749ac368206cb0edb307f3a05b
Nhat <60876500+UpperMoon0@users.noreply.github.com>
2023-07-26 00:21:33 +0700

    Full adult animal and datapack

--- src/main/java/com/nhat/biotech/Blocks/NetTrapBlock.java
+++ src/main/java/com/nhat/biotech/Blocks/NetTrapBlock.java
@@ -15,14 +15,13 @@ import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.material.MapColor;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.Nullable;
 
 public class NetTrapBlock extends Block implements EntityBlock {
     public NetTrapBlock() {
-        super(BlockBehaviour.Properties.copy(Blocks.OAK_PLANKS));
+        super(BlockBehaviour.Properties.copy(Blocks.OAK_PLANKS).noOcclusion());
     }
 
     @Override
